04a588bcefcb12c86e41f8816a6c578c397b8939
Megatus Prime <wyvernc16@example.com>
2025-07-02 17:09:25 +0800

    Not Red

---
+++ Capstone_EcoLodge_Demo/src/main/java/com/example/Capstone_EcoLodge_Demo/CapstoneEcoLodgeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Capstone_EcoLodge_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneEcoLodgeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneEcoLodgeDemoApplication.class, args);
+	}
+
+}
---
+++ Capstone_EcoLodge_Demo/src/test/java/com/example/Capstone_EcoLodge_Demo/CapstoneEcoLodgeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Capstone_EcoLodge_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneEcoLodgeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
