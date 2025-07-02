908780db877abe7102c5243ccd4b0cf6997952f0
klngcAt <killer.hydra50@gmail.com>
2025-07-02 17:12:55 +0800

    class products

---
+++ Capstone_EcoLodge_Demo/src/main/java/com/example/Capstone_EcoLodge_Demo/products.java
@@ -0,0 +1,14 @@
+package com.example.Capstone_EcoLodge_Demo;
+
+@RestController
+public class products {
+    @RequestMapping("/")
+    public String Greet(){
+        
+        return "Welcome to Hell";
+    }
+    @RequestMapping("/about")
+    public String about(){
+        return "There is nothing Here!!";
+    }
+}
