c97979e7972333788e6ee4cb4737ca4decda6c3c
Megatus Prime <wyvernc16@example.com>
2025-07-02 17:00:50 +0800

    Red Codes

---
+++ Capstone_EcoLodge/src/test/java/com/example/Capstone_EcoLodge/CapstoneEcoLodgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Capstone_EcoLodge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneEcoLodgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
