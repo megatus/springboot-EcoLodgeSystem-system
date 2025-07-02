a620cee2f3125d47d48be3d7941d63b12589e4a9
klngcAt <killer.hydra50@gmail.com>
2025-07-02 17:27:03 +0800

    product model service controller

---
+++ Capstone_EcoLodge_Demo/src/main/java/com/example/Capstone_EcoLodge_Demo/Model/Product.java
@@ -0,0 +1,18 @@
+package com.WebApp.WebApplication.Model;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Component
+public class Product {
+    private int prodID;
+    private String Name;
+    private int Price;
+    public Product() {
+        
+    }
+}
